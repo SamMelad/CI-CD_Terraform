e5279f6d94ad96fa4c4173d7256a625602d5a111
Samuel Francis <samuelmaled32@gmail.com>
2024-03-25 12:36:18 +0200

    CI/CD Terraform

--- Modules/ec2/ec2.tf
+++ Modules/ec2/ec2.tf
@@ -31,6 +31,11 @@ resource "aws_instance" "ec2-instance" {
   # To create EC2 instance in public subnet
   subnet_id = var.subnet_id
 
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCLkMQrpUERem0297ma7FDJKTJ/abHGkHpr4WxKhiJCfcvVrAJeGguWziFtQxs4BpX0mz6gv5FWqhuXwZoeUKHt0tihy4RJWV9uoSkWMoH8OTvw6vAg77mFk8UbxszfS1zYey7W1gAH2HVqJYhboImoNGZf+8LgsGS27AoSp40sBUY6YuzMXNsei8R674DN1IeL/z1vgcuT5Kcgudn56VWEH1LsE9qOc5IAD9DssffRMYkCrQBRGpXrcVsqGIHTD9UcuD3vDvxvGnYhbBFaHkL+jGq6TzEohPgDUnMAEtDupUsYtR2x7+CRKQbl73WWU1/R0sYKT8Yd8Hmp4GlgQatn CI/CD" >> /home/ec2-user/.ssh/authorized_keys
+              EOF
+
   # Security group that is created above 
   security_groups = [ aws_security_group.sg.id ]
   
